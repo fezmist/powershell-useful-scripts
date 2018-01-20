5a0f309b025792abc16f0e852d4ba33d65182e32
fezmist <faiz.mistry@gmail.com>
2018-01-20 19:16:02 +0000

    added rename bulk files with seq number logic

--- rename-files.ps1
+++ rename-files.ps1
@@ -8,3 +8,14 @@ get-childitem | Where-Object {$_.Name -like 'old-filename.part*'} | Rename-Item 
 
 ##rename command2
 Get-ChildItem | Rename-Item -NewName {$_.DirectoryName +"\" + $_.Name.Replace("learning-d3-","")}
+
+##Rename files in bulk with number sequence
+##cd "C:\users\faiz\Pictures\My Screen Shots\aws-sa-a-exam3"
+$count = 0
+Get-ChildItem | Where-Object { $_.Extension -eq ".PNG"} | Sort-Object -Property LastWriteTime -Descending |
+ForEach-Object { 
+    $count++
+    $oldName = $_.Name
+    $newName = "Q" + $count + ".PNG"
+    $_ | Rename-Item -NewName { $_.DirectoryName + "\" + $_.Name.Replace($oldName, $newName) }
+    }
